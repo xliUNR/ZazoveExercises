c1d50dca5238c82b200b3f993417db022461a305
xliUNR <xli@nevada.unr.edu>
2019-06-24 22:14:48 -0700

    started test code for json read and made example data

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,16 @@
+!#/usr/bin/python3
+import sys
 import json
+import unittest
 
 # First will implement a test for taking in the JSON file.
+def test_jsonFileOpen( fileName ):
+    with open( fileName ) as jsonFile:
+        data = json.load( jsonFile )
+        #print(  )
+
+with open( sys.argv[1] ) as jsonFile:
+    data = json.load( jsonFileName )
 
 # read json file in
 
