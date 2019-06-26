24c26a8b574f86824ad7eaf632b1b2d4bc43da80
xliUNR <xli@nevada.unr.edu>
2019-06-25 19:21:13 -0700

    created and tested json opening function

--- exercise1.py
+++ exercise1.py
@@ -1,16 +1,36 @@
-!#/usr/bin/python3
+#!/usr/bin/python3
 import sys
 import json
 import unittest
 
 # First will implement a test for taking in the JSON file.
+# Print out values to check 
 def test_jsonFileOpen( fileName ):
     with open( fileName ) as jsonFile:
         data = json.load( jsonFile )
-        #print(  )
+        print( data[ 'parAmount'] )
+        print( data[ 'yearsUntilMaturity'] )
+        print( data[ 'coupon'] )
+        print( data[ 'couponFrequency' ] )
+        print( data[ 'couponTiming' ] )
+        print( data[ 'yieldCurve' ] )
 
-with open( sys.argv[1] ) as jsonFile:
-    data = json.load( jsonFileName )
+# function for opening json files
+def jsonFileOpen( fileName ):
+    with open( fileName ) as jsonFile:
+        return json.load( jsonFile )
+
+
+def main():
+    #with open( sys.argv[1] ) as jsonFile:
+    #data = json.load( jsonFileName )
+    #test_jsonFileOpen( sys.argv[1] )
+    inputData = jsonFileOpen( sys.argv[1] )
+    #print(data)
+
+if __name__ == '__main__':
+    main()
+
 
 # read json file in
 
