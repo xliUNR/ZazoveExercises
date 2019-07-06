2240fb9b90ade76d33535f9a0f394c7f7083f221
xliUNR <xli@nevada.unr.edu>
2019-07-05 23:01:32 -0700

    unit test for input checking

--- exercise2/ex2_unittestScript.py
+++ exercise2/ex2_unittestScript.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 import unittest
-
+import optionsFunctions as opt
 
 class InputTest( unittest.TestCase ):
 
-    # test for floats
+    # tests for float checker
+    def test_floatTypeCheckCorrect(self):
+        self.assertEqual( opt.floatTypeCheck( 1.25, 'num' ), True )
+    
+    def test_floatTypeCheckIncorrect(self):
+        self.assertEqual( opt.floatTypeCheck( 'hello', 'num' ), False )
+
+
+    #test for integer checker
+    def test_intTypeCheckCorrect(self):
+        self.assertEqual( opt.intTypeCheck( 1, 'num' ), True )
+
+    def test_intTypeCheckIncorrect(self):
+        self.assertEqual( opt.intTypeCheck( 'hello', 'num' ), False )
+
+    # tests for negative values check
+    def test_negativeCheckNegVals(self):
+        self.assertEqual( opt.negativeCheck( -10, 'num' ), 1 )
+
+    def test_negativeCheckZero(self):
+        self.assertEqual( opt.negativeCheck( 0, 'num' ), 1 )
+
+    def test_negativeCheckPosVals(self):
+        self.assertEqual( opt.negativeCheck( 10, 'num' ), 0 )
+
+
+if __name__ == '__main__':
+    unittest.main()
